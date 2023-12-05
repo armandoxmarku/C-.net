1798f42fe7913cc6874a062ff1ecf9517fa07b1e
armandoxmarku <armandomarkudr@gmail.com>
2023-12-05 20:52:48 +0100

    sms

---
+++ CSharpDotNET/RazorFun/controller/HomeController.cs
@@ -0,0 +1,12 @@
+// This brings all the MVC features we need to this file
+using Microsoft.AspNetCore.Mvc;
+// Be sure to use your own project's namespace here! 
+namespace RazorFun.Controllers;   
+public class HomeController : Controller 
+{
+    [HttpGet("")]
+     public ViewResult Index()        
+    {            
+     return View("Index");  
+    }  
+}
