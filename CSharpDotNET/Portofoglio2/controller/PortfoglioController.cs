5e539785b9c4dd0653e7d0f6eb6c0e50476fff29
armandoxmarku <armandomarkudr@gmail.com>
2023-12-07 21:37:27 +0100

    sms

---
+++ CSharpDotNET/Portofoglio2/controller/PortfoglioController.cs
@@ -0,0 +1,20 @@
+// This brings all the MVC features we need to this file
+using Microsoft.AspNetCore.Mvc;
+// Be sure to use your own project's namespace here! 
+namespace Portfoglio.Controllers;   
+public class PortfoglioController : Controller  
+{
+    [HttpGet("")]
+    public ViewResult Index(){
+        return View("index");
+    }
+    [HttpGet("projects")]
+    public ViewResult Projects(){
+        return View("projects");
+    }
+    [HttpGet("contact")]
+    public ViewResult Contacts(){
+        return View("contacts");
+
+    }
+}
